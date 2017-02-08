92b0b756d02de40530f02fbfbb91c71de928f723
wzhy <wzhyno1@163.com>
2017-02-08 10:18:32 +0800

    添加ActivityHome及相文件和配置

---
+++ app/src/main/java/com/techfit/mdofbehavior/ActivityHome.java
@@ -0,0 +1,13 @@
+package com.techfit.mdofbehavior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
